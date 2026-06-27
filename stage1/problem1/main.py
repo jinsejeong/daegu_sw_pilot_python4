12058686c2695c834ede28a0ea717d09061a6e85
jinsejeong <tpwlswjd913@naver.com>
2026-06-27 13:33:47 +0900

    필요없는 파일 삭제

--- stage1/problem1/main.py
+++ stage1/problem1/main.py
@@ -4,7 +4,7 @@ try :
     with open("mission_computer_main.log","r") as f:
         print(f.read())
 except FileNotFoundError:
-    print("File not found. Please check the file path and try again.")  
+    print("File not found. Please ensure the file exists in the specified path.")  
 except UnicodeDecodeError:
     print("Error decoding the file. Please ensure the file is in the correct format.")
 except Exception as e:
